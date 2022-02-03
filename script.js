a2a800fcdab6e251ac197da296724388f3fa53f9
ddderi <maxime830@live.fr>
2022-02-03 06:15:45 +0100

    JS done

--- script.js
+++ script.js
@@ -1,20 +1,44 @@
+//
+
 const btc = document.getElementById('btc')
+const eth = document.getElementById('eth')
+const bnb = document.getElementById('bnb')
+const ada = document.getElementById('ada')
 const body = document.getElementById('body')
 const newdiv = document.createElement('div')
 
 
+//Event listener for the 4 choices of crypto that we can display on the website.
+
 btc.addEventListener('click', function(e){
-    fetchBTC()
+    seek = event.target.textContent
+    fetchData()
 })
 
+eth.addEventListener('click', function(e){
+    seek = event.target.textContent
+    fetchData()
+})
 
-function fetchBTC(){
+bnb.addEventListener('click', function(e){
+    seek = event.target.textContent.replace(' ', '')
+    fetchData()
+})
 
-fetch("https://coingecko.p.rapidapi.com/simple/price?ids=bitcoin&vs_currencies=usd&include_last_updated_at=false&include_market_cap=true&include_24hr_change=true&include_24hr_vol=false"
+ada.addEventListener('click', function(e){
+    seek = event.target.textContent
+    fetchData()
+})
+
+//function to fetch the cryptocurrency I am looking for.
+
+function fetchData(){
+fetch(`https://coingecko.p.rapidapi.com/simple/price?ids=${seek}&vs_currencies=usd`
 , {
 	"method": "GET",
 	"headers": {
-		
+		"x-rapidapi-host": "coingecko.p.rapidapi.com",
+		"x-rapidapi-key": 'd84f0708afmsh5ca0134fa6807b8p1c4acejsn553f05ea523d'
 	}
 }
  ) 
@@ -25,19 +49,18 @@ fetch("https://coingecko.p.rapidapi.com/simple/price?ids=bitcoin&vs_currencies=u
 });
 }
 
+// function which treats the response/function fetchData and put the data in the DOM
+
 function pushData(bitbit){
     let constt = document.createElement('p')
-            constt.innerHTML = btc.innerText
+            constt.innerHTML = seek
             newdiv.append(constt)
     let bitcoinmess = Object.entries(bitbit)
     bitcoinmess.forEach(Tbit => {
         newO = Tbit[1]
         for (let property in newO){
-                
-                
                 const ulparent = document.createElement('ul')
                 let li = document.createElement('li')
-                
                 li.innerHTML = `${property}: ${newO[property]}`;
                 ulparent.append(li)
                 newdiv.append(ulparent)
@@ -50,8 +73,8 @@ function pushData(bitbit){
             button.innerHTML = 'Back to the main menu'
             button.classList = 'button'
             button.addEventListener('click', function(e){
-                newdiv.innerHTML = '';
-                newdiv.remove()
+            newdiv.innerHTML = '';
+            newdiv.remove()
             })
             
     })
